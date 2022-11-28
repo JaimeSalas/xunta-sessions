f6a8f604cffb12078ef4a55fc5aa8e04efa769b8
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-11-28 19:11:09 +0100

    basic form

--- 03-basic-app/src/app.tsx
+++ 03-basic-app/src/app.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LoginPage } from './login';
 import { ListPage } from './list';
 import { DetailPage } from './detail';
 
 export const App = () => {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/list" element={<ListPage />} />
         <Route path="/detail" element={<DetailPage />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 };
 
--- 03-basic-app/src/login.tsx
+++ 03-basic-app/src/login.tsx
@@ -1,18 +1,49 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+// user: admin
+// password: test
 export const LoginPage: React.FC = () => {
   const navigate = useNavigate();
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-  const handleNavigate = () => {
-    navigate("/list");
+  const isValidCredentials = (username: string, password: string) =>
+    username === "admin" && password === "test";
+
+  const handleNavigate = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    if (isValidCredentials(username, password)) {
+      navigate("/list");
+    } else {
+        alert('Username / Password wrong... admin / test');
+    }
   };
 
   return (
-    <>
+    <form onSubmit={handleNavigate}>
       <h2>Hello from Login Page</h2>
-      {/* <Link to="/list">Navigate to list page</Link> */}
-      <button onClick={handleNavigate}>Login</button>
-    </>
+      <div>
+        <div>
+          <label>Username: </label>
+          <input
+            type="text"
+            // value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            // value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+      {/* <button onClick={handleNavigate}>Login</button> */}
+      <button type="submit">Login</button>
+    </form>
   );
 };
