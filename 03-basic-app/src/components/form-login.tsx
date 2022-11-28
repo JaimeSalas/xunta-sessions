2e283eb20dbe223031016ae0a1d9176d5d4ca0ac
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-11-28 19:45:47 +0100

    added form refactor

---
+++ 03-basic-app/src/components/form-login.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+interface FormControlProps {
+  label: string;
+  type: string;
+  onChange: (value: string) => void;
+}
+
+const FormControl: React.FC<FormControlProps> = ({ label, type, onChange }) => {
+  return (
+    <div>
+      <label>{label}: </label>
+      <input type={type} onChange={(e) => onChange(e.target.value)} />
+    </div>
+  );
+};
+
+interface FormProps {
+  onSubmit: (username: string, password: string) => void;
+}
+
+export const FormLogin: React.FC<FormProps> = ({ onSubmit }) => {
+  const [credentials, setCredentials] = React.useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(credentials.username, credentials.password);
+  } 
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormControl
+        label="Username"
+        type="text"
+        onChange={(username) => {
+          setCredentials({
+            ...credentials,
+            username,
+          });
+        }}
+      />
+      <FormControl
+        label="Password"
+        type="password"
+        onChange={(password) => {
+          setCredentials({ ...credentials, password });
+        }}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
--- 03-basic-app/src/login.tsx
+++ 03-basic-app/src/login.tsx
@@ -1,49 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { FormLogin } from "./components/form-login";
 
-// user: admin
-// password: test
 export const LoginPage: React.FC = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
 
   const isValidCredentials = (username: string, password: string) =>
     username === "admin" && password === "test";
 
-  const handleNavigate = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    
+  const handleNavigate = (username: string, password: string) => {
     if (isValidCredentials(username, password)) {
       navigate("/list");
     } else {
-        alert('Username / Password wrong... admin / test');
+      alert("Username / Password wrong... admin / test");
     }
   };
 
-  return (
-    <form onSubmit={handleNavigate}>
-      <h2>Hello from Login Page</h2>
-      <div>
-        <div>
-          <label>Username: </label>
-          <input
-            type="text"
-            // value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password: </label>
-          <input
-            type="password"
-            // value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-      </div>
-      {/* <button onClick={handleNavigate}>Login</button> */}
-      <button type="submit">Login</button>
-    </form>
-  );
+  return <FormLogin onSubmit={handleNavigate} />;
 };
