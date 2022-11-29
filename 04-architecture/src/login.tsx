7dd87bff675edb7502ac13224d37e9cb1825e7d4
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-11-29 17:36:02 +0100

    added architecture project

---
+++ 04-architecture/src/login.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FormLogin } from "./components/form-login";
+
+export const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const isValidCredentials = (username: string, password: string) =>
+    username === "admin" && password === "test";
+
+  const handleNavigate = (username: string, password: string) => {
+    if (isValidCredentials(username, password)) {
+      navigate("/list");
+    } else {
+      alert("Username / Password wrong... admin / test");
+    }
+  };
+
+  return <FormLogin onSubmit={handleNavigate} />;
+};
---
+++ 04-architecture/webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
+    },
+    entry: {
+        app: ["./index.tsx", "./styles.css"]
+    },
+    devtool: 'eval-source-map',
+    stats: 'errors-only',
+    output: {
+        filename: '[name].[chunkhash].js',
+        publicPath: '/'
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(png|jpg)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html'
+        }),
+    ]
+};
