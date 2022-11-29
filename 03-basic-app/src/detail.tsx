45d2b74ce4b48ec65ab4fbd3d52c0bb7f2621e4a
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-11-29 16:16:23 +0100

    exercise details

--- 03-basic-app/src/detail.tsx
+++ 03-basic-app/src/detail.tsx
@@ -3,7 +3,20 @@ import { Link, useParams } from "react-router-dom";
 
 export const DetailPage: React.FC = () => {
   const { id } = useParams();
-  
+
+  // https://api.github.com/users/<id>
+  // https://api.github.com/users/ajoulin
+
+  /*
+  {
+    "login": "ajoulin",
+    "id": 4343641,
+    "name": null,
+    "company": null,
+    "bio": null
+  }
+  */
+
   return (
     <>
       <h2>Hello from Detail</h2>
