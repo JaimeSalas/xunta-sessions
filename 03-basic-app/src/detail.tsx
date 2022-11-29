36970bd6ac5650cefa6994d9d0b9408175c4ea95
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-11-29 17:00:34 +0100

    added basic app

--- 03-basic-app/src/detail.tsx
+++ 03-basic-app/src/detail.tsx
@@ -1,8 +1,51 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 
+interface MemberDetail {
+  login: string;
+  id: number | null;
+  name: string;
+  company: string;
+  bio: string;
+  type: "pro" | "standard";
+}
+
+// type MemberDetailPartial = Partial<MemberDetail>;
+// Pick -> toma las propiedades que te interesen
+// Omit -> toma todas las propiedades menos las que omites
+
+const createDefaultMember = (): MemberDetail => ({
+  login: "",
+  bio: "",
+  id: null,
+  company: "",
+  name: "",
+  type: "standard",
+});
+
+const handleFetchJsonResponse = (r: Response) => {
+  if (r.ok) {
+    return r.json();
+  }
+};
+
+const retrieveMember = (login: string): Promise<MemberDetail> =>
+  fetch(`https://api.github.com/users/${login}`)
+    .then(handleFetchJsonResponse)
+    .then((v) => {
+      console.dir(v);
+      return { ...v, type: "standard" };
+    });
+
 export const DetailPage: React.FC = () => {
+  const [member, setMember] = React.useState<MemberDetail>(createDefaultMember);
   const { id } = useParams();
+
+  React.useEffect(() => {
+    retrieveMember(id)
+      .then(setMember)
+      .catch(console.error);
+  }, []);
 
   // https://api.github.com/users/<id>
   // https://api.github.com/users/ajoulin
@@ -20,7 +63,12 @@ export const DetailPage: React.FC = () => {
   return (
     <>
       <h2>Hello from Detail</h2>
-      <h3>User Id: {id}</h3>
+      <p>id: {member.id}</p>
+      <p>login: {member.login}</p>
+      <p>name: {member.name}</p>
+      <p>company: {member.company}</p>
+      <p>bio: {member.bio}</p>
+      <p>type: {member.type}</p>
       <Link to="/list">back to list page</Link>
     </>
   );
