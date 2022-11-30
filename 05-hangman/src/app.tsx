92a0b86184413d9ec404f122ceb35ee0b9d2535a
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-11-30 19:25:04 +0100

    basic game working

--- 05-hangman/src/app.tsx
+++ 05-hangman/src/app.tsx
@@ -1,29 +1,43 @@
 import React from "react";
-import { getRandomWord } from './random-word.service';
-import { PlaceholderComponent, SecretWordComponent, InputGuessLetterComponent } from './components';
+import { getRandomWord } from "./random-word.service";
+import {
+  SecretWordComponent,
+  InputGuessLetterComponent,
+} from "./components";
 
 export const App = () => {
   const [guessCount, setGuessCount] = React.useState(0);
+  const [word, setWord] = React.useState("");
+  const [guessLetter, setGuessLetter] = React.useState("");
 
   React.useEffect(() => {
     const t = getRandomWord();
     console.log(t);
+    setWord(t);
   }, []);
 
   return (
     <div>
-      <PlaceholderComponent letter="R" show={false} />
-      <SecretWordComponent word={getRandomWord()} />
-      <InputGuessLetterComponent onCheck={(val) => {
-        console.log(val);
-        const updated = guessCount + 1;
-        setGuessCount(updated)
-      }} />
+      <SecretWordComponent
+        word={word}
+        guessLetter={guessLetter}
+        onGuessed={(val) => {
+          console.log("gueeeeessssed!!", val);
+        }}
+      />
+      <InputGuessLetterComponent
+        onCheck={(val) => {
+          console.log(val);
+          if (!word.includes(val)) {
+            const updated = guessCount + 1;
+            setGuessCount(updated);
+          }
+          setGuessLetter(val);
+        }}
+      />
       <div>
         <span>{guessCount}</span>
       </div>
     </div>
-
   );
 };
-
--- 05-hangman/src/components/secret-word.component.tsx
+++ 05-hangman/src/components/secret-word.component.tsx
@@ -3,17 +3,53 @@ import { PlaceholderComponent } from "./placeholder.component";
 
 interface Props {
   word: string;
+  guessLetter: string;
+  onGuessed?: (guessed: boolean) => void;
 }
 
 const stringToArray = (s: string): string[] => [...s];
 
+interface SecretLetter {
+  letter: string;
+  show: boolean;
+}
+
 export const SecretWordComponent: React.FC<Props> = (props: Props) => {
-  // 'hola' -> ['h', 'o',....]
-  const { word } = props;
+  const { word, guessLetter, onGuessed } = props;
+
+  const [secretWord, setSecretWord] = React.useState<SecretLetter[]>([]);
+
+  React.useEffect(() => {
+    setSecretWord(stringToArray(word).map((l) => ({ letter: l, show: false })));
+  }, [word]);
+
+  React.useEffect(() => {
+    if (guessLetter !== "") {
+      const updated = secretWord.map((sl) => {
+        if (sl.show) {
+          return sl;
+        }
+
+        return {
+          ...sl,
+          show: sl.letter === guessLetter,
+        };
+      });
+      setSecretWord(updated);
+    }
+  }, [guessLetter]);
+
+  React.useEffect(() => {
+    const guessed = secretWord.length > 0 && secretWord.every((v) => v.show);
+    if (onGuessed && guessed) {
+      onGuessed(guessed);
+    }
+  }, [secretWord]);
+
   return (
-    <div style={{ display: 'flex' }}>
-      {stringToArray(word).map((l) => (
-        <PlaceholderComponent letter={l} show={false} />
+    <div style={{ display: "flex" }}>
+      {secretWord.map(({ letter, show }, i) => (
+        <PlaceholderComponent key={i} letter={letter} show={show} />
       ))}
     </div>
   );
