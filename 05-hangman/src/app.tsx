666b0ab6c7dcb354e6ec62f108249da670cb2572
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-11-30 18:35:49 +0100

    last cut

--- 05-hangman/src/app.tsx
+++ 05-hangman/src/app.tsx
@@ -3,6 +3,8 @@ import { getRandomWord } from './random-word.service';
 import { PlaceholderComponent, SecretWordComponent, InputGuessLetterComponent } from './components';
 
 export const App = () => {
+  const [guessCount, setGuessCount] = React.useState(0);
+
   React.useEffect(() => {
     const t = getRandomWord();
     console.log(t);
@@ -12,7 +14,14 @@ export const App = () => {
     <div>
       <PlaceholderComponent letter="R" show={false} />
       <SecretWordComponent word={getRandomWord()} />
-      <InputGuessLetterComponent onCheck={(val) => console.log(val)} />
+      <InputGuessLetterComponent onCheck={(val) => {
+        console.log(val);
+        const updated = guessCount + 1;
+        setGuessCount(updated)
+      }} />
+      <div>
+        <span>{guessCount}</span>
+      </div>
     </div>
 
   );
